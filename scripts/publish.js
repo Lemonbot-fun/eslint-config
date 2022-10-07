398f22fb100a3a229d70c391192d4b68cd33d5f1
jason-chang <chaoyue.chang@gmail.com>
2022-10-07 21:34:43 +0800

    chore: 发版

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,43 +1,165 @@
-const { readdirSync } = require('fs');
+
+const { existsSync, readdirSync } = require('fs');
 const { exec } = require('child-process-promise');
+const { prompt, Confirm } = require('enquirer');
 const path = require('path');
+const semver = require('semver');
 const ac = require('ansi-colors');
 
+const projRoot = process.cwd();
 const pkgsDir = path.join(process.cwd(), 'packages');
-
 let npmCmd = 'pnpm';
 if (process.platform === 'win32') {
   npmCmd = 'pnpm.cmd';
 }
 
-(async () => {
-  const res = await exec('git status --porcelain');
+const releaseTypeList = ['major', 'premajor', 'minor', 'preminor', 'patch', 'prepatch'];
 
-  if (res.stdout.toString().trim().length) {
-    console.error('\nError: Unclean working tree. Commit or stash changes first.');
+const mapedPkgInfoList = new Map();
+const sortedPkgInfoList = [];
+
+// read & sort pkg info
+(() => {
+  const dirs = readdirSync(pkgsDir);
+  const pkgInfoList = [];
+  dirs.forEach(p => {
+    const pkgDir = path.resolve(pkgsDir, p);
+    const pkgConfPath = path.resolve(pkgDir, 'package.json');
+    const pkg = require(pkgConfPath);
+    const pkgInfo = {
+      name: pkg.name,
+      dir: pkgDir,
+      deps: Object
+        .entries(pkg.dependencies)
+        .filter(([_, ver]) => (/^workspace:/.test(ver)))
+        .map(([n]) => n),
+      sort: 0,
+      included: 0,
+      conf: pkg,
+    };
+    mapedPkgInfoList.set(pkg.name, pkgInfo);
+    pkgInfoList.push(pkgInfo);
+  });
+  pkgInfoList.forEach(i => {
+    i.deps.forEach(d => (mapedPkgInfoList.get(d).included++));
+  });
+  while (pkgInfoList.length) {
+    const cur = pkgInfoList.shift();
+    if (cur.deps.length === 0) {
+      sortedPkgInfoList.push(cur);
+      continue;
+    }
+
+    if (cur.deps.some(item => {
+      return !sortedPkgInfoList.includes(mapedPkgInfoList.get(item));
+    })) {
+      pkgInfoList.push(cur);
+    } else {
+      sortedPkgInfoList.push(cur);
+    }
+  }
+})();
+
+let pIndex = 0;
+async function progressItem() {
+  if (pIndex === sortedPkgInfoList.length) return;
+
+  const curPkg = sortedPkgInfoList[pIndex];
+  process.chdir(curPkg.dir);
+
+  // = ---------------------------- = ask version = ---------------------------- =
+  const relativeDir = path.relative(projRoot, curPkg.dir);
+  try {
+    await exec(`git diff --exit-code ${relativeDir}`);
+    pIndex++;
+    await progressItem();
+    return;
+  } catch (e) {}
+
+  let version = curPkg.conf.version;
+  if (!version) {
+    throw new Error('no version in package.json');
+  }
+
+  let curTypeList = Array.from(releaseTypeList);
+  let maxReleaseType = '';
+  let maxReleaseTypeIndex = releaseTypeList.length - 1;
+  if (curPkg.deps.length) {
+    maxReleaseTypeIndex = curPkg.deps.reduce((pre, cur) => {
+      const depReleaseType = mapedPkgInfoList.get(cur).releaseType;
+      return Math.min(pre,
+        depReleaseType
+          ? releaseTypeList.indexOf(depReleaseType)
+          : releaseTypeList.length - 1,
+      );
+    }, maxReleaseTypeIndex);
+    maxReleaseType = releaseTypeList[maxReleaseTypeIndex];
+    curTypeList = curTypeList.slice(0, maxReleaseTypeIndex + 1);
+    if (maxReleaseType.includes('pre')) {
+      curTypeList = curTypeList.filter(i => /^pre/.test(i));
+    }
+  }
+
+  let releaseType = 'major';
+  if (!maxReleaseTypeIndex) {
+    version = semver.inc(version, releaseType, 'pre');
+    console.log(`Set "${curPkg.name}" version to ${version}`);
+  } else {
+    const choices = curTypeList
+      .reverse()
+      .map(item => `${item}: ${semver.inc(version, item, 'pre')}`);
+
+    ({ version } = await prompt({
+      name: 'version',
+      type: 'select',
+      message: `Select "${curPkg.name}" release version`,
+      choices,
+    }));
+
+    ([releaseType, version] = version.split(': '));
+
+    const confirm = new Confirm({ name: 'isSure', message: `Set "${curPkg.name}" version to ${version} - are you sure?` });
+    const answer = await confirm.run();
+    !answer && process.exit();
+  }
+
+  curPkg.releaseType = releaseType;
+  curPkg.releaseVersion = version;
+
+  pIndex++;
+  await progressItem();
+}
+
+async function applyVersionAndPublish() {
+  if (pIndex === sortedPkgInfoList.length) return;
+  const cur = sortedPkgInfoList[pIndex];
+  // set version
+  try {
+    if (!cur.releaseVersion || cur.releaseVersion === cur.conf.version) {
+      pIndex++;
+      await applyVersionAndPublish();
+      return;
+    }
+
+    process.chdir(cur.dir);
+    await exec(`${npmCmd} version ${cur.releaseVersion} --no-git-tag-version`);
+    const relativeDir = path.relative(projRoot, cur.dir);
+
+    await exec(`${npmCmd} publish --access=publish --no-git-checks --registry=https://registry.npmjs.org/`);
+    console.log(`${ac.green('✔')} "${relativeDir}" publish complete.`);
+
+    pIndex++;
+    await applyVersionAndPublish();
+  } catch (e) {
+    console.error(e.stderr || e.stdout || e);
     process.exit(1);
   }
+}
 
-  const dirs = readdirSync(pkgsDir);
-
-  let index = 0;
-  async function progressItem() {
-    if (index === dirs.length) return;
-
-    const cur = dirs[index];
-    const relativeDir = path.join('packages', cur);
-    try {
-      await exec(`git diff --exit-code ${relativeDir}`);
-      index++;
-      progressItem();
-      return;
-    } catch (e) {}
-
-    process.chdir(path.join(pkgsDir, cur));
-    exec(`pnpm publish --access=publish --repository=https://www.npmjs.com/`);
-
-    console.log(`${ac.green('✔')} "${relativeDir}" publish complete.`);
-  }
-
+(async () => {
   await progressItem();
+
+  pIndex = 0;
+  await applyVersionAndPublish();
 })();
+
